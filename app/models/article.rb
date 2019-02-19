e44f401eb2317b244d299bf98f0b24ffd09c36ae
Griffin Tang <gltang@andrew.cmu.edu>
2019-02-18 22:39:05 -0500

    fixed routes and typos

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :category
-  valdiates_presence_of :title, :content
+  validates_presence_of :title, :content
 
   scope :active, 	   -> { where('active = ?', true) }
   scope :alphabetical, -> { order('title') }
